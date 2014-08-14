ad889620eeef44083b8c3615241d1f841a719f6c
Zhangle <iamchuzhiyan@gmail.com>
2014-08-14 20:17:25 +0800

    Add a new method named bubble sort.

---
+++ BubbleSort/src/BubbleSort.java
@@ -0,0 +1,49 @@
+/**
+ * Created by Happy on 2014/8/14.
+ */
+public class BubbleSort {
+    /**
+     * To print the array
+     *
+     * @param a the array which is be printed
+     * @param n the element's number in tha array a
+     * @param i the index
+     */
+    public static void print(int a[], int n, int i) {
+        System.out.print(i + ":");
+        for (int j = 0; j < n; j++) {
+            System.out.print(a[j] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Sorting array in a bubble sort way
+     *
+     * @param a the array which is be sorted
+     * @param n the element's number in tha array a
+     */
+    public static void bubbleSort(int a[], int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                //if a[j] < a[j+1], then swap them
+                if (a[j] > a[j + 1]) { //The result is the biggest one becomes the last one
+                    int temp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = temp;
+                }
+            }
+            print(a, n, i);
+        }
+    }
+
+    /**
+     * the main function
+     *
+     * @param args
+     */
+    public static void main(String args[]) {
+        int a[] = {3, 1, 5, 7, 2, 4, 9, 6};
+        bubbleSort(a, 8);
+    }
+}
