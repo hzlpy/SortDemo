5bd2182c39c4863f4a25ec47cf21683e5dd90cda
Zhangle <iamchuzhiyan@gmail.com>
2014-08-19 21:09:14 +0800

    Add three more implement ways in insert sort.

--- StraightInsertSort/src/StraightInsetSort.java
+++ StraightInsertSort/src/StraightInsetSort.java
@@ -39,6 +39,54 @@ public class StraightInsetSort {
         }
     }
 
+    public static void insertSort3(int a[], int n) {
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                int j = i - 1;
+                int x = a[i];
+                //a[i] = a[i-1];
+                while (j >= 0 && x < a[j]) {
+                    a[j + 1] = a[j];
+                    j--;
+                }
+                a[j + 1] = x;
+            }
+            print(a, n, i);
+        }
+    }
+
+    public static void insertSort1(int a[], int n) {
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                int temp = a[i - 1];
+                a[i - 1] = a[i];
+                a[i] = temp;
+            }
+            int j = i - 1;
+            while ((j - 1) >= 0 && a[j] < a[j - 1]) {
+                int temp = a[j - 1];
+                a[j - 1] = a[j];
+                a[j] = temp;
+                j--;
+            }
+            print(a, n, i);
+        }
+    }
+
+    public static void insertSort2(int a[], int n) {
+        for (int i = 1; i < n; i++) {
+            while ((i - 1) >= 0 && a[i] < a[i - 1]) {
+                int temp = a[i - 1];
+                a[i - 1] = a[i];
+                a[i] = temp;
+                i--;
+            }
+            print(a, n, i);
+        }
+    }
+
+
+
     /**
      * the main function
      *
@@ -46,6 +94,7 @@ public class StraightInsetSort {
      */
     public static void main(String args[]) {
         int a[] = {3, 1, 5, 7, 2, 4, 9, 6};
-        InsertSort(a, 8);
+//        InsertSort(a, 8);
+        insertSort3(a, 8);
     }
 }
